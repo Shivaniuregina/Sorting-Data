b456121cf238acd42a2da607fa5547eff6440e74
Shivani <shivaniielts@gmail.com>
2020-08-10 21:12:41 -0600

    Updates

---
+++ precache-manifest.397f30bb1ab7a3e3295165f5c8f47004.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3611efc064ca1eb8eaa27211118acb52",
+    "url": "/Sorting-Data/index.html"
+  },
+  {
+    "revision": "06a4daaccd051ecf0135",
+    "url": "/Sorting-Data/static/css/main.e2129432.chunk.css"
+  },
+  {
+    "revision": "ae4e3fa89f350a97ec16",
+    "url": "/Sorting-Data/static/js/2.6d8871a7.chunk.js"
+  },
+  {
+    "revision": "06a4daaccd051ecf0135",
+    "url": "/Sorting-Data/static/js/main.5cc16f82.chunk.js"
+  },
+  {
+    "revision": "f70c1eca6a270e2e26b9",
+    "url": "/Sorting-Data/static/js/runtime-main.26e54142.js"
+  }
+]);
